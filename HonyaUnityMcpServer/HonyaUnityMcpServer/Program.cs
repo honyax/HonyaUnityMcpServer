c75e2be7b08db426e7f912cfbe8cf279ebb83df3
honyax <honyax@gmail.com>
2025-04-13 19:52:23 +0900

    テスト用プロジェクトを作成し、プロジェクトの構成をもろもろ修正

--- HonyaUnityMcpServer/HonyaUnityMcpServer/Program.cs
+++ HonyaUnityMcpServer/HonyaUnityMcpServer/Program.cs
@@ -32,28 +32,6 @@ public static class Program
 
     static async Task Main(string[] args)
     {
-        // コマンドライン引数でクライアントテストを実行するかどうかを判定
-        if (args.Contains("--run-client-test"))
-        {
-            Console.WriteLine("WebSocket クライアントテストを実行します...");
-            await WebSocketClientTest.MainTest();
-            Console.WriteLine("クライアントテストが終了しました。");
-            return; // クライアントテストを実行したらサーバーは起動しない
-        }
-#if false
-        var res = await CreatePrimitiveGameObjectTool.HumsCreatePrimitiveGameObject(PrimitiveType.Sphere, "MySphere");
-        var options = new JsonSerializerOptions
-        {
-            IncludeFields = true,
-            WriteIndented = true,
-        };
-        _logger.Log($"resStr:{JsonSerializer.Serialize(res, options)}");
-        var res2 = await SetTransformTool.HumsSetTransform(res.instanceId, 10, 20, 30, 0, 0, 0, 2, 3, 5);
-        _logger.Log($"resStr2:{JsonSerializer.Serialize(res2, options)}");
-#endif
-
-        // --- 以下は通常のサーバー起動処理 ---
-
         var builder = Host.CreateApplicationBuilder(args);
         builder.Logging.ClearProviders();
         builder.Logging.AddConsole(consoleLogOptions =>
