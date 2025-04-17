839b5a21a180c7dd07469af99bfc86b356ea3835
honyax <honyax@gmail.com>
2025-04-18 01:50:07 +0900

    アタッチコンポーネントツールを作成

---
+++ HonyaUnityMcpServer/HonyaUnityMcpServer/src/Tools/AttachComponentTool.cs
@@ -0,0 +1,26 @@
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using HonyaUnityMcpServer.Libs;
+using HonyaMcp;
+
+namespace HonyaUnityMcpServer.Tools;
+
+[McpServerToolType]
+public static class AttachComponentTool
+{
+    [McpServerTool, Description("コンポーネントを GameObject にアタッチする")]
+    public static async Task<AttachComponentResponse> HumsAttachComponent(
+        [Description("アタッチするコンポーネント名")]
+        string componentName,
+        [Description("アタッチ先の GameObject 名")]
+        string gameObjectName)
+    {
+        var response = await HonyaMcpClient.SendMessage<AttachComponentRequest, AttachComponentResponse>("AttachComponent", new AttachComponentRequest
+        {
+            componentName = componentName,
+            gameObjectName = gameObjectName
+        });
+
+        return response;
+    }
+}
